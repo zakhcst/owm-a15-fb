b995f32774108d4364cdddffe703a8b8940c64d6
ztt <ztchernev@gmail.com>
2020-01-11 01:15:23 +0000

    Landing page/view update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,8 +9,45 @@ import { ConstantsService } from '../../services/constants.service';
 export class HomeComponent implements OnInit {
   constants = ConstantsService;
   loaded = false;
+  data = [
+    {
+      version : 8,
+      db: 'Firebase RTDB',
+      hosted: 'Firebase',
+      routedLink: '/toolbar/forecast-flex',
+      sourceLink:'https://github.com/zakhcst/owm-a8-fb'
+    },
+    {
+      version : 7,
+      db: 'Firebase RTDB',
+      hosted: 'Firebase',
+      hostedLink: 'https://owm-a7-fb.firebaseapp.com/',
+      sourceLink:'https://github.com/zakhcst/owm-a7-fb'
+    },
+    {
+      version : 7,
+      db: 'Firebase RTDB',
+      hosted: 'GCP AE(f1)',
+      hostedLink: 'https://owm-a7-fb.appspot.com/',
+      sourceLink:'https://github.com/zakhcst/owm-a7-fb'
+    },
+    {
+      version : 6,
+      db: 'Firebase RTDB',
+      hosted: 'Firebase',
+      hostedLink: 'https://owm-a7-fb.firebaseapp.com/',
+      sourceLink:'https://github.com/zakhcst/owm-a7-fb'
+    },
+    {
+      version : 1.6,
+      db: 'OWM Requests',
+      hosted: 'Firebase',
+      hostedLink: 'https://owm-a7-fb.firebaseapp.com/',
+      sourceLink:'https://github.com/zakhcst/owm-a7-fb'
+    }
+  ]
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.loaded = true;
