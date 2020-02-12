da4b3846222141375f94c697f5dc17dd85d7e673
ztt <ztchernev@gmail.com>
2020-02-12 03:33:32 +0000

    Modules refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
-
 import { RequiredModules } from './modules/required.module';
+import { AdditionalModules } from './modules/additional.module';
 
 import { AppComponent } from './app.component';
 import { AppSnackBarInnerComponent } from './components/app-snack-bar-inner/app-snack-bar-inner.component';
@@ -12,15 +9,14 @@ import { AppRoutingModule } from './modules/routing.module';
 import { AppErrorHandlerService } from './services/app-error-handler.service';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HttpInterceptorService } from './services/http-interceptor.service';
+
 @NgModule({
   declarations: [AppComponent, AppSnackBarInnerComponent],
 
   imports: [
     HttpClientModule,
     RequiredModules,
-    NgxsReduxDevtoolsPluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot(),
-    NgxsRouterPluginModule.forRoot(),
+    AdditionalModules,
     AppRoutingModule
   ],
   exports: [RequiredModules],
---
+++ src/app/modules/additional.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+
+@NgModule({
+  imports: [
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsRouterPluginModule.forRoot(),
+  ],
+  exports: [
+    NgxsReduxDevtoolsPluginModule,
+    NgxsLoggerPluginModule,
+    NgxsRouterPluginModule,
+  ]
+})
+export class AdditionalModules { }
