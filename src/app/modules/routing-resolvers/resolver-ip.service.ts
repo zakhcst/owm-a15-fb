41d1aaed562d07dc7e41608281476106d455fb11
ztt <ztchernev@gmail.com>
2020-09-12 00:41:14 +0100

    Update ip service - refresh ip on reconnect

--- src/app/modules/routing-resolvers/resolver-ip.service.ts
+++ src/app/modules/routing-resolvers/resolver-ip.service.ts
@@ -2,24 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { Resolve } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
-
-import { GetBrowserIpService } from '../../services/get-browser-ip.service';
-import { Store } from '@ngxs/store';
-import { SetStatusIpState } from '../../states/app.actions';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ResolverIpService implements Resolve<string> {
-  constructor(private _store: Store, private _ip: GetBrowserIpService) { }
+export class ResolverIpService implements Resolve<any> {
 
-  resolve(): Observable<string> | Observable<never> {
-    return this._ip.getIPv4().pipe(
-      catchError((errIp) => {
-        return of('255.255.255.255');
-      }),
-      switchMap((ip: string) => this._store.dispatch([new SetStatusIpState(ip)]))
-    );
+  resolve(): Observable<any> | Observable<never> {
+    return of(true);
   }
 }
--- src/app/services/get-browser-ip.service.ts
+++ src/app/services/get-browser-ip.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ConstantsService } from './constants.service';
-import { of, Observable, throwError } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
+import { of, Observable, throwError, Subscription } from 'rxjs';
+import { switchMap, catchError, take, filter, tap } from 'rxjs/operators';
 import { ErrorsService } from './errors.service';
+import { Select, Store } from '@ngxs/store';
+import { AppStatusState } from '../states/app.state';
+import { SetStatusIpState } from '../states/app.actions';
 @Injectable({
   providedIn: 'root',
 })
 export class GetBrowserIpService {
+  getIPv4Subscription: Subscription;
 
-  constructor(private _http: HttpClient, private _errors: ErrorsService) {}
+  @Select(AppStatusState.connected) connected$: Observable<boolean>;
+
+  constructor(private _http: HttpClient, private _errors: ErrorsService, private _store: Store) {
+    this.refreshIpOnConnect();
+  }
 
   getIPv4() {
-    return this.requestIPv4().pipe(switchMap(this.validateIPv4.bind(this)));
+    return this.requestIPv4().pipe(
+      switchMap(this.validateIPv4.bind(this)),
+      catchError((errIp) => {
+        return of('255.255.255.255');
+      })
+    );
   }
 
   requestIPv4(): Observable<string> {
     return this._http.get(ConstantsService.getIpUrl, { responseType: 'text' }).pipe(
+      take(1),
       catchError((err) => {
         this.setIPv4Error(err.message || err);
         return throwError('--ip-error-connection');
@@ -37,5 +51,23 @@ export class GetBrowserIpService {
       userMessage: 'Connection or service problem',
       logMessage: 'GetBrowserIpService: getIPv4: ' + error,
     });
+  }
+
+  refreshIpOnConnect() {
+    this.connected$
+      .pipe(
+        filter((connected) => {
+          return connected;
+        })
+      )
+      .subscribe(() => {
+        console.log('refreshIpOnConnect');
+        if (this.getIPv4Subscription) {
+          this.getIPv4Subscription.unsubscribe();
+        }
+        this.getIPv4Subscription = this.getIPv4().subscribe((ip: string) => {
+          this._store.dispatch([new SetStatusIpState(ip)]);
+        });
+      });
   }
 }
