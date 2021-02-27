7ea0dd7c3e3c044312ec734b49b940836da93b24
ztt <ztchernev@gmail.com>
2021-02-27 12:46:56 +0000

    Refactoring AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
-import { PresenceService } from './services/presence.service';
 import { Observable, Subscription } from 'rxjs';
-import { SwUpdate } from '@angular/service-worker';
-import { Select, Store } from '@ngxs/store';
-import { SetStatusBuildInfo, SetStatusShowLoading, SetStatusUpdatesAvailable } from './states/app.actions';
+import { Select } from '@ngxs/store';
 import { ConstantsService } from './services/constants.service';
+import { AppInitService } from './services/app-init.service';
 import { AppStatusState } from './states/app.state';
 import { debounceTime } from 'rxjs/operators';
 @Component({
@@ -20,93 +17,19 @@ export class AppComponent implements OnDestroy {
   @Select(AppStatusState.showLoading) showLoading$: Observable<boolean>;
 
   constructor(
-    private _router: Router,
-    private _presence: PresenceService,
-    private _updates: SwUpdate,
-    private _store: Store
+    private appInitService: AppInitService
   ) {
-    this.initCss();
-    this.setSubscribeOnRouterEvents();
-    this.setSubscribeOnConnected();
-    this.startListenerOnAway();
-    this.setSubscribeOnUpdates();
     this.setSubscribeDebounceLoadingActions();
   }
 
-  initCss() {
-    [
-      'iconArrowWindDirection',
-      'iconsMeasures',
-      'iconsWeather'
-    ].forEach((property) => {
-      document.documentElement.style.setProperty(`--${property}Url`, `url("../../../${ConstantsService[property]}")`);
-    });
-    [
-      'showDetailPressure',
-      'showDetailWind',
-      'showDetailHumidity',
-      'showDetailSecondary'
-    ].forEach((property) => {
-      document.documentElement.style.setProperty('--' + property, this._store.selectSnapshot(AppStatusState[property]) ? 'flex' : 'none');
-    });
-  }
-
   setSubscribeDebounceLoadingActions() {
-    const subscriptionDespatchLoadingActions = this.showLoading$
-      .pipe(debounceTime(ConstantsService.loadingDispatechesDebounceTime_ms))
-      .subscribe((loading) => {
-        this.loading = loading;
-      });
-    this.subscriptions.add(subscriptionDespatchLoadingActions);
-  }
-
-  setSubscribeOnUpdates() {
-    this._store.dispatch(new SetStatusUpdatesAvailable(false));
-    this.subscriptions.add(
-      this._updates.available.subscribe((event) => {
-        console.log('Current version is', event.current);
-        console.log('New available version is', event.available);
-        this._store.dispatch(new SetStatusUpdatesAvailable(true));
-        const buildInfo = {
-          current: (event.current?.appData as any)?.buildInfo,
-          available: (event.available?.appData as any)?.buildInfo
-        };
-        this._store.dispatch(new SetStatusBuildInfo(buildInfo));
-      })
-    );
-  }
-
-  startListenerOnAway() {
-    this._presence.updateOnAway();
-  }
-
-  setSubscribeOnConnected() {
-    this.subscriptions.add(
-      this._presence.updateOnConnected().subscribe((connected) => console.log('AppComponent connected', connected))
-    );
-  }
-
-  setSubscribeOnRouterEvents() {
-    this.subscriptions = this._router.events.subscribe((routerEvent: Event) => {
-      this.checkRouterEvent(routerEvent);
+    this.subscriptions = this.showLoading$
+    .pipe(debounceTime(ConstantsService.loadingDispatechesDebounceTime_ms))
+    .subscribe((loading) => {
+      this.loading = loading;
     });
-  }
-
-  checkRouterEvent(routerEvent: Event) {
-    if (routerEvent instanceof NavigationStart) {
-      this._store.dispatch(new SetStatusShowLoading(true));
     }
-
-    if (
-      routerEvent instanceof NavigationEnd ||
-      routerEvent instanceof NavigationCancel ||
-      routerEvent instanceof NavigationError
-    ) {
-      this._store.dispatch(new SetStatusShowLoading(false));
-    }
-  }
-
-  ngOnDestroy() {
+      ngOnDestroy() {
     if (this.subscriptions) {
       this.subscriptions.unsubscribe();
     }
---
+++ src/app/services/app-init.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
+import { Store } from '@ngxs/store';
+import { Subscription } from 'rxjs';
+import { SetStatusBuildInfo, SetStatusShowLoading, SetStatusUpdatesAvailable } from '../states/app.actions';
+import { AppStatusState } from '../states/app.state';
+import { ConstantsService } from './constants.service';
+import { PresenceService } from './presence.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppInitService {
+  subscriptions: Subscription;
+  
+  constructor(
+    private _router: Router,
+    private _presence: PresenceService,
+    private _updates: SwUpdate,
+    private _store: Store
+  ) { 
+    this.initCss();
+    this.setSubscribeOnRouterEvents();
+    this.setSubscribeOnConnected();
+    this.startListenerOnAway();
+    this.setSubscribeOnUpdates();
+  }
+
+  initCss() {
+    [
+      'iconArrowWindDirection',
+      'iconsMeasures',
+      'iconsWeather'
+    ].forEach((property) => {
+      document.documentElement.style.setProperty(`--${property}Url`, `url("../../../${ConstantsService[property]}")`);
+    });
+    [
+      'showDetailPressure',
+      'showDetailWind',
+      'showDetailHumidity',
+      'showDetailSecondary'
+    ].forEach((property) => {
+      document.documentElement.style.setProperty('--' + property, this._store.selectSnapshot(AppStatusState[property]) ? 'flex' : 'none');
+    });
+  }
+
+  setSubscribeOnUpdates() {
+    this._store.dispatch(new SetStatusUpdatesAvailable(false));
+    this.subscriptions.add(
+      this._updates.available.subscribe((event) => {
+        console.log('Current version is', event.current);
+        console.log('New available version is', event.available);
+        this._store.dispatch(new SetStatusUpdatesAvailable(true));
+        const buildInfo = {
+          current: (event.current?.appData as any)?.buildInfo,
+          available: (event.available?.appData as any)?.buildInfo
+        };
+        this._store.dispatch(new SetStatusBuildInfo(buildInfo));
+      })
+    );
+  }
+
+  startListenerOnAway() {
+    this._presence.updateOnAway();
+  }
+
+  setSubscribeOnConnected() {
+    this.subscriptions.add(
+      this._presence.updateOnConnected().subscribe((connected) => console.log('AppComponent connected', connected))
+    );
+  }
+
+  setSubscribeOnRouterEvents() {
+    this.subscriptions = this._router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event) {
+    if (routerEvent instanceof NavigationStart) {
+      this._store.dispatch(new SetStatusShowLoading(true));
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this._store.dispatch(new SetStatusShowLoading(false));
+    }
+  }
+
+}
